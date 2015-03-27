d3c34e0fb95fc477f24f0dadb9029e13c50c6ebb
Kirsten Korevaar <kirsten@eml.cc>
2015-03-27 12:06:53 +0700

    Add C14

--- chapter01_C13-28.py
+++ chapter01_C13-28.py
@@ -12,5 +12,21 @@ def reverse_list(data):
 		new_list.append(data[-index])
 	return new_list
 
+
+# C-1.14 Write a short Python function that takes a sequence of integer
+# values and determines if there is a distinct pair of numbers in the 
+# sequence whose product is odd.
+
+def odd_sequence(data):
+	for index in range(len(data)):
+		if is_odd(data[index] * data[index + 1]):
+			return index, index + 1
+
+
+def is_odd(k):
+     return k % 2 != 0
+
+
 if __name__ == "__main__":
-	print reverse_list([1, 2, 3, 4])
+	print reverse_list([1, 2, 3, 4]) == [4, 3, 2, 1]
+	print odd_sequence([1, 2, 3, 7]) == (2, 3)
