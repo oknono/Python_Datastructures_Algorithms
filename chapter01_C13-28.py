0ff13345ffe7ce93b270f90112d7a42c79da0b81
Kirsten Korevaar <kirsten@eml.cc>
2015-03-27 19:08:52 +0700

    add ex 24

--- chapter01_C13-28.py
+++ chapter01_C13-28.py
@@ -1,49 +1,40 @@
-# C-1.13 - Write a pseudo-code description of
-# a function that reverses a list of n integers,
-# so that the numbers are listed in the opposite order
-# than they were before, and compare this method to an equivalent Python
-# function for doing the same thing.
-
-# python build-in : print name_list.reverse()
-
+# C-1.13 
+# print name_list.reverse()
 
 def reverse_list(data):
-	new_list = []
-    for index in range(1, len(data) + 1):
-        new_list.append(data[-index])
-    return new_list
+	return data
 
+print reverse_list([1, 2, 3, 4])
 
 # C-1.14 Write a short Python function that takes a sequence of integer
 # values and determines if there is a distinct pair of numbers in the
 # sequence whose product is odd.
 
 
-def odd_sequence(data):
-    for index in range(len(data)):
-        if is_odd(data[index] * data[index + 1]):
-            return index, index + 1
+# def odd_sequence(data):
+#    for index in range(len(data)):
+#        if is_odd(data[index] * data[index + 1]):
+#            return index, index + 1
 
 
-def is_odd(k):
-    return k % 2 != 0
+# def is_odd(k):
+#   return k % 2 != 0
 
 
 # C-1.15 Write a Python function that takes a sequence of numbers
 # and determines if all the numbers are different from each other
 # (that is, they are distinct)
 
-
-def unique(data):
-    new_list = []
-    for index in range(len(data)):
-        if data[index] in new_list:
-            return False
-        else:
-            new_list.append(data[index])
-    return True
-
-
+#def unique(data):
+#    new_list = []
+#    for index in range(len(data)):
+#        if data[index] in new_list:
+#            return False
+#        else:
+#            new_list.append(data[index])
+#   return True
+#
+#
 # C-1.16 
 # def scale(data, factor):
 #     for j in range(len(data)):
@@ -65,8 +56,30 @@ def unique(data):
 
 # print [chr(n) for n in range(97, 123)]
 
-if __name__ == "__main__":
-    print reverse_list([1, 2, 3, 4]) == [4, 3, 2, 1]
-    print odd_sequence([1, 2, 3, 7]) == (2, 3)
-    print unique([1, 2, 3, 4]) == True
-    print unique([1, 2, 3, 4, 2, 0]) == False
+# C-1.20 Shuffle a list using only randint(begin, end)
+# start with a list - reassign each item to a new index.
+# We start with n possible indices, and these shrink
+# The issue I have is that I can't say between 1 and 5, but not three
+# If I need to reuse indices, best way to do so. Every element needs to be in new list
+# TO DO
+
+# C-1.21 TO DO
+
+# C-1.22
+#def multiply_list(data1, data2):
+#	data3 = []
+#	for index in range(len(data1)):
+#		data3.append(data1[index]*data2[index])
+#	return data3
+#
+
+# C-1.24
+
+def count_vowel(word):  
+    return sum(1 for letter in word if letter in 'aeoui')
+#if __name__ == "__main__":
+#    print reverse_list([1, 2, 3, 4]) == [4, 3, 2, 1]
+#    print odd_sequence([1, 2, 3, 7]) == (2, 3)
+#    print unique([1, 2, 3, 4]) == True
+#    print unique([1, 2, 3, 4, 2, 0]) == False
+#    print multiply_list([1,2,3,4,5],[2,3,4,5,6])
