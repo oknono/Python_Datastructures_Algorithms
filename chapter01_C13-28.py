624e101199f9060d69627e316d315d3bc18cbee4
Kirsten Korevaar <kirsten@eml.cc>
2015-04-02 21:14:59 +0700

    add placeholder 16 and 17 (to do)

--- chapter01_C13-28.py
+++ chapter01_C13-28.py
@@ -39,3 +39,14 @@ def no_duplicates(data):
 # print no_duplicates([])
 # print no_duplicates([1, 2, 3, 4, 5, 6])
 # print no_duplicates([1, 2, 3, 4, 5, 6, 4])
+
+# C-1.16  and C-1.17 _ TODO
+# In this function we manipulate a list element - changing the list.
+# List and scale(list,factor) have same ID
+# With function with exercise, a None is returned
+
+# def scale(data, factor):
+#    for j in range(len(data)):
+#        data[j] *= factor
+a
+# print scale([1, 20, 300, 4000], 2)
